32883c2d752a453afbc8ace499423711e1517e35
taiti09 <taitiabe1997@gmail.com>
2023-09-18 11:08:46 +0900

    ✅ fix : domain model

--- backend/src/lib/domain/entity.ts
+++ backend/src/lib/domain/entity.ts
@@ -109,3 +109,25 @@ class TaskUpdatedAt {
         this.Value = new Date().toISOString();
     }
 }
+
+export class Response {
+    readonly status: string;
+    readonly code: Number;
+
+    constructor(status:string, code:Number){
+        this.status = status;
+        this.code = code;
+    }
+}
+
+export class RequestParam {
+    readonly taskName: TaskName;
+    readonly taskDesc: TaskDesc;
+    readonly taskStatus: TaskStatus;
+
+    constructor(name:string,desc:string,status:Number) {
+        this.taskName = new TaskName(name);
+        this.taskDesc = new TaskDesc(desc);
+        this.taskStatus = new TaskStatus(status);
+    }
+}
